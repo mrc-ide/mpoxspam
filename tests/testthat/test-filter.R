282c5dda5d7f8797bdd8d546fe23349e331b8676
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-11-21 16:35:50 +0000

    Add smoke test

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -1,9 +1,13 @@
 test_that("can run filter", {
   pars <- reference_pars()
   dat <- filter_data()
-  filter <- m4_2_filter(dat, n_particles = 5000, n_threads = 1, seed = 1L)
+  set.seed(1)
+  filter <- m4_2_filter(dat, n_particles = 100, n_threads = 1, seed = 1L)
   ## pomp:   :  207s
   ## 8 threads: 0.896 user, 0.114 elapsed (1725x pomp)
   ## 1 thread:  0.665 user, 0.664 elapsed ( 259x pomp)
-  system.time(filter$run(pars))
+  ll <- filter$run(pars)
+  ## Smoke test, will need updating on changes to basically anything,
+  ## but guard against unexpected changes.
+  expect_equal(ll, -453.653978547233)
 })

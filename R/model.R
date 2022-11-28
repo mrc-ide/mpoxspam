6a37590b0d81a10947756f79fc4ea2bf753b91c5
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-11-28 17:21:38 +0000

    Export functions, add docs

--- R/model.R
+++ R/model.R
@@ -1,3 +1,30 @@
+##' @name model
+##' @title Pair approximation transmission model of Mpox virus
+##'
+##' @description The basic model; we may add more or adapt this one
+##'   over time.
+##'
+##' @export model
+NULL
+
+##' Compare observed and modelled data from the basic model. This
+##' conforms to the mcstate interface.
+##'
+##' @title Compare observed and modelled data for the basic model
+##'
+##' @param state State vector for the end of the current day. This is
+##'   assumed to be filtered following [model_index()] so contains
+##'   rows corresponding to new infections and scaled time.
+##'
+##' @param observed Observed data. This will be a list with elements
+##'   `Ytravel`, `Yendog` and `Yunk`.
+##'
+##' @param pars A list of parameters
+##'
+##' @return A vector of log likelihoods, the same length as the number
+##'   of particles (the number of columns in the modelled state)
+##'
+##' @export
 model_compare <- function(state, observed, pars) {
   ## Unpack modelled:
   newI <- state["newI", ]
@@ -34,6 +61,18 @@ model_compare <- function(state, observed, pars) {
   t1 + t2
 }
 
+##' Index of "interesting" elements for the model. This function
+##' conforms to the mcstate interface.
+##'
+##' @title Index of model variables
+##'
+##' @param info The result of running the `$info()` method on an
+##'   initialised [basic] model
+##'
+##' @return A list with element `run`, indicating the locations of the
+##'   compartments used in [model_compare]
+##'
+##' @export
 model_index <- function(info) {
   list(run = c(newI = info$index$newI,
                newIseed = info$index$newIseed,
@@ -42,6 +81,20 @@ model_index <- function(info) {
 }
 
 
+##' Create a particle filter; just a convenience function around some
+##' mcstate defaults.
+##'
+##' @title Create particle filter
+##'
+##' @param data Data to use
+##'
+##' @param ... Additional arguments to [mcstate::particle_filter]'s constructor
+##'
+##' @param use_compiled_compare Logical, indicating if we should use
+##'   the compiled compare function rather than [model_compare]. This
+##'   will likely be faster if several threads are used.
+##'
+##' @export
 model_filter <- function(data, ..., use_compiled_compare = FALSE) {
   compare <- if (use_compiled_compare) NULL else model_compare
   mcstate::particle_filter$new(data, model, ...,

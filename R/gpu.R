e4ba1a72cc7b58b0fc7a362cb0a4e2b263de2657
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2022-11-29 10:42:15 +0000

    Export and fix gpu compilation wrapper

--- R/gpu.R
+++ R/gpu.R
@@ -1,9 +1,20 @@
+##' Compile model for GPU
+##'
+##' @title Compile model for GPU
+##'
+##' @param ... Additional arguments to [odin.dust::odin_dust()]
+##'
+##' @param real_type Name of real type to use, probably "float"
+##'
+##' @param gpu Configuration, passed through to [odin.dust::odin_dust()]
+##'
+##' @export
 compile_gpu <- function(..., real_type = "float", gpu = TRUE) {
   ## Avoid weird errors by making sure these versions are ok here;
   ## later we can drop this and rely on the DESCRIPTION.
   loadNamespace("odin.dust", versionCheck = list(op = ">=", version = "0.2.28"))
   loadNamespace("lostturnip", versionCheck = list(op = ">=", version = "0.1.2"))
 
-  path <- system.file("odin/m4_2.R", package = "monkeyspam", mustWork = TRUE)
+  path <- system.file("odin/model.R", package = "mpoxspam", mustWork = TRUE)
   odin.dust::odin_dust_(path, real_type = real_type, gpu = gpu, ...)
 }

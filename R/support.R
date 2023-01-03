f6bd04932ba0c75017838b9776a84536adb3a7e2
Lilith Whittles <lwhittles@users.noreply.github.com>
2023-01-03 15:36:22 +0000

    import functions from stats

--- R/support.R
+++ R/support.R
@@ -1,3 +1,4 @@
+##' @importFrom stats rexp
 ll_betabinom <- function(data_a, data_b, model_a, model_b, rho, exp_noise) {
   n <- length(model_a)
   if (is.na(data_a) || is.na(data_b) || (data_a == 0 && data_b == 0)) {
@@ -8,7 +9,7 @@ ll_betabinom <- function(data_a, data_b, model_a, model_b, rho, exp_noise) {
   ldbetabinom(data_a, data_b, model_a + da, model_b + db, rho)
 }
 
-
+##' @importFrom stats dnbinom
 ll_nbinom <- function(data, model, kappa, exp_noise) {
   if (is.na(data)) {
     return(numeric(length(model)))

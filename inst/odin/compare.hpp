19acdbb4d5a2d5effb9dc67e6e91ff15abef9ebf
Lilith Whittles <lwhittles@users.noreply.github.com>
2023-01-11 17:29:19 +0000

    compiled compare is NegBin/Bin

--- inst/odin/compare.hpp
+++ inst/odin/compare.hpp
@@ -13,20 +13,21 @@ compare(const typename T::real_type * state,
   const real_type model_newI = odin(newI);
   const real_type delta = calc_delta(odin(delta0), odin(delta1),
                                      odin(delta_slope), odin(time));
-  const real_type cases = dust::math::ceil(model_newI  * delta);
-  const real_type model_newIseed = odin(newIseed);
+  const real_type cases = dust::math::ceil(model_newI * delta);
   const real_type Yknown = data.Ytravel + data.Yendog;
   const real_type Y = dust::math::ceil(Yknown + data.Yunk);
-  const real_type model_newIendog = model_newI - model_newIseed;
+  const real_type model_p = odin(newIseed) / model_newI;
 
   real_type ret = 0;
   if (!std::isnan(Y)) {
     real_type ll_cases = ll_nbinom(Y, cases, odin(kappa_cases),
                                    odin(exp_noise), rng_state);
-    real_type ll_travel = ll_betabinom(data.Ytravel, data.Yendog,
-                                       model_newIseed, model_newIendog,
-                                       odin(rho_travel), odin(exp_noise),
-                                       rng_state);
+
+    real_type ll_travel = 0;
+    if (Yknown > 0) {
+      ll_travel = model_newI == 0 ? -inf<real_type> : dust::density::binomial(dust::math::ceil(data.Ytravel), dust::math::ceil(Yknown), model_p, true);
+    }
+
     ret = ll_cases + ll_travel;
   }
 

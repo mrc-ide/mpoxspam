3147224a37db985aae46467842b2baf9e4cf9dea
Lilith Whittles <lwhittles@users.noreply.github.com>
2023-02-14 17:37:12 +0000

    add test of update_theta_vacc_4.3 - currently failing on edge cases (prop_vacc_targetted = 0/1)

--- tests/testthat/helper-monkeyspam.R
+++ tests/testthat/helper-monkeyspam.R
@@ -3,7 +3,7 @@ evaluate_pgf <- function(f, x) {
 }
 
 
-reference_update_theta_vacc <- function(theta_vacc, vacc_amt) {
+reference_update_theta_vacc_4.2 <- function(theta_vacc, vacc_amt) {
   f <- function(x) (2809 + 2000 * x + 95 * x^2) / 4904
   g <- function(x) (2943 + 1009 * x + 477 * x^2 + 475 * x^3) / 4904
   hshape <- 0.26
@@ -18,6 +18,19 @@ reference_update_theta_vacc <- function(theta_vacc, vacc_amt) {
   rf <- function(lntheta) {
     f(exp(lntheta)) * g(exp(lntheta)) * h(exp(lntheta)) - p1
   }
+  o <- optimise(lower = -1e4, upper = 0, f = of)
+  r <- uniroot(lower = -1e4, upper = 0, f = rf)
+  c(exp(o$minimum), exp(r$root))
+}
+
+reference_update_theta_vacc_4.3 <- function(prop_vacc_targetted) {
+  p1 <- max(0.0001, 1 - prop_vacc_targetted)
+
+  hshape <- 0.26
+  hrate <- 1.85 * 7
+  h <- function(x) (1 - log(x) / hrate)^(-hshape)
+  of <- function(lntheta) (h(exp(lntheta)) - p1)^2
+  rf <- function(lntheta) h(exp(lntheta)) - p1
   o <- optimise(lower = -1e4, upper = 0, f = of)
   r <- uniroot(lower = -1e4, upper = 0, f = rf)
   c(exp(o$minimum), exp(r$root))
